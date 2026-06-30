9c67db2b9f0d0c45d57c5400a6945abfcc9da18e
The Volto Corporation <thevoltocorporation@proton.me>
2026-06-30 17:59:51 +0200

    Form Fix

--- script.js
+++ script.js
@@ -72,12 +72,3 @@ navButtons.forEach((button, index) => {
 
 });
 
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", function(e){
-
-    e.preventDefault();
-
-    alert("Message sent!");
-
-});
