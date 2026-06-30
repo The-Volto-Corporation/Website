bff356efc1a237bfe7a6ee26ee56062f6307dc07
The Volto Corporation <thevoltocorporation@proton.me>
2026-06-30 23:03:52 +0200

    fix images?

--- projects/FOF/FOFscript.js
+++ projects/FOF/FOFscript.js
@@ -13,27 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const conceptArt = [
         {
-            image: "images/concept1.png",
+            image: "./images/concept1.png",
             title: "Ramon Face Concepts",
             description: "When designing Roman’s face I tried to aim for the feeling of someone soft that didn’t allow the cruel world to turn him stone cold. Even with his sharp features I kept the eyes soft and tired to convey his kindness. I also wanted to show how the two years of the apocalypse have changed him as a person."
         },
         {
-            image: "images/concept2.png",
+            image: "./images/concept2.png",
             title: "Owen Face Concepts",
             description: "The strong leader of the group of survivors the player meets. I wanted his face to be square with more manly features to give off that unmovable wall vibe. I also wanted to show how he cares much more about his duty than himself by giving him a lot more hair to show that he doesn’t really focus on his appearance."
         },
         {
-            image: "images/concept3.png",
+            image: "./images/concept3.png",
             title: "Audrey Face Concepts",
             description: "A true soul untamed. I wanted Audrey to be wild, carefree and someone that finds a thrill that finds in killing the undead. I made her hair messy and wild with a smirk constantly on her face. I wanted to create someone who thrived in the new world and creating her was the perfect way to show a much more wild charter in this world."
         },
         {
-            image: "images/concept4.png",
+            image: "./images/concept4.png",
             title: "Olivia Face Concepts",
             description: "Soft and gentle. That is how I wanted Olivia’s personality to be and how she conveys herself as a person. Giving her softer features and a cute hairstyle to show that she isn’t a threat to anyone. I designed her features around the fact that she is the nurse of the survival group and softer features give of a caretaker personality. "
         },
         {
-            image: "images/concept5.png",
+            image: "./images/concept5.png",
             title: "Lonan Face Concepts",
             description: "Lonan was supposed to look as if the apocalypse hadn't affected him. Not in a way that makes him handle it with ease. I wanted him to be pretty and well kept as if the apocalypse has never happened and hasn't really changed what he does everyday."
         }
